362fe36427f85a43c7fb3643d2eacc4556ee2dec
varnithraj <aero.mksamy@gmail.com>
2025-11-27 11:29:20 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,5 @@
+#greetings people of earth
+print("my name is varnithranj and my brother's is harnithraj")
+
+#welcome people of earth to ong planet
+print("my hobby is to play, read,hit my brother")
